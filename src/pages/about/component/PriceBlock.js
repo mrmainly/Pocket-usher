eeabf0cda746d80844f4f756fcbf6d9dc7c29a4c
ernest <ernest_everstov@mail.ru>
2021-04-09 13:01:05 +0300

    added background and video block

---
+++ src/pages/about/component/PriceBlock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+
+const PriceBlock = () => {
+    return (
+        <Container>
+            SDFSDF
+        </Container>
+    )
+}
+
+export default PriceBlock
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,13 +3,32 @@ import Layout from '../../components/layout/Layout'
 import StartedBlog from './component/StartedBlog'
 import GroupOne from './component/GroupOne'
 import GroupTwo from './component/GroupTwo'
+import VideoBlock from './component/VideoBlock'
+import AnswerGroup from './component/AnswerGroup'
+import PriceBlock from './component/PriceBlock'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        backgroundImage: `url(${"/image/Element/Vector6.png"})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+    },
+}));
+
 
 const About = () => {
+    const classes = useStyles()
     return (
         <Layout>
             <StartedBlog />
             <GroupOne />
             <GroupTwo />
+            <VideoBlock />
+            <div className={classes.container}>
+                <AnswerGroup />
+                <PriceBlock />
+            </div>
         </Layout>
     )
 }
